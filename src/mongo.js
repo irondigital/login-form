d8cf9ac5d1c01353e10b77e1ee6a1da77255bc31
irondigital <milanbalas123@gmail.com>
2024-04-07 11:57:08 +0530

    first commit

---
+++ src/mongo.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1/LoginFormPractice",{
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+})
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+})
+
+const logInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+
+module.exports=LogInCollection
